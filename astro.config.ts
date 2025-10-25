b837e365354f9597cdb69ffd6d3b3d9555923d79
Yahia <yn7599@gmail.com>
2025-10-25 20:54:52 +0300

    fonts

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,24 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Cairo",
+        cssVariable: "--font-cairo",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Roboto Mono",
+        cssVariable: "--font-roboto-mono",
+      },
+    ],
   },
 });
